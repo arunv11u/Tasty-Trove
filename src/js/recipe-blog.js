0df0dc017cff15c2171fe4aabbc5fbb24b9f192e
Arun Varadharajalu <arunvaradharajalu@gmail.com>
2023-12-14 12:08:04 -0500

    Add comments to recipe blog and recipe page.

--- src/js/recipe-blog.js
+++ src/js/recipe-blog.js
@@ -1,58 +1,67 @@
+// Enable strict mode for better error handling and code quality
 'use strict';
 
 $(() => {
+	// Extract the 'name' parameter from the URL query string
 	const params = new URLSearchParams(window.location.search);
 	const name = params.get("name");
 
+	// Fetch recipe data from the JSON file
 	fetch("./json/recipes.json")
 		.then(response => response.json())
 		.then(recipes => {
+			// Find the recipe with the matching name
 			const recipe = recipes.find(recipe => recipe.name === name)
 
+			// Set the recipe image source
 			const recipeImage = $("#recipe-image");
 			recipeImage.attr("src", recipe.image);
 
+			// Set the recipe creation date
 			const recipeCreatedOn = $("#recipe-created-on");
 			recipeCreatedOn.html(recipe.createdOn);
 
+			// Set the recipe name
 			const recipeName = $("#recipe-name");
 			recipeName.html(recipe.name);
 
+			// Set the recipe preparation duration
 			const recipePrepDuration = $("#recipe-prep-duration");
 			recipePrepDuration.html(`Prep: ${recipe.prepDuration} mins`);
 
+			// Set the recipe cooking duration
 			const recipeCookDuration = $("#recipe-cook-duration");
 			recipeCookDuration.html(`Cook: ${recipe.cookingDuration} mins`);
 
+			// Set the recipe yields information
 			const recipeYields = $("#recipe-yeild");
 			recipeYields.html(`Yields: ${recipe.yields} Servings`);
 
+			// Set the recipe difficulty level
 			const recipeLevel = $("#recipe-level");
 			recipeLevel.html(recipe.level);
 
+			// Set the recipe preparation steps
 			const recipesElement = $("#recipes");
-
 			let recipeSteps = ``;
 			recipe.steps.forEach((step, index) => {
-
+				// Format each preparation step with an index and description
 				recipeSteps += `
 				<div class="single-preparation-step d-flex">
-								<h4>0${index + 1}.</h4>
-								<p>${step}
-								</p>
-							</div>`;
+					<h4>0${index + 1}.</h4>
+					<p>${step}</p>
+				</div>`;
 			});
-
 			recipesElement.html(recipeSteps);
 
+			// Set the recipe ingredients
 			const ingredientsElement = $("#ingredients");
-
-			let ingredents = ``;
-			recipe.ingredients.forEach((ingredent, index) => {
-
-				ingredents += `<label class="custom-control">${ingredent}</label>`;
+			let ingredients = ``;
+			recipe.ingredients.forEach((ingredient, index) => {
+				// Create a label for each ingredient using a custom control style
+				ingredients += `<label class="custom-control">${ingredient}</label>`;
 			});
-
-			ingredientsElement.html(ingredents);
-		}).catch(err => console.error(err));
+			ingredientsElement.html(ingredients);
+		})
+		.catch(err => console.error(err)); // Handle any errors that occur during the fetch operation
 });
--- src/js/recipe.js
+++ src/js/recipe.js
@@ -1,15 +1,20 @@
+// Enable strict mode for better error handling and code quality
 'use strict';
 
 $(() => {
+	// Fetch recipes from the JSON file when the page loads
 	fetch("./json/recipes.json")
 		.then(response => response.json())
 		.then(recipes => {
+			// Select the recipes container element
 			const recipesElement = $("#recipes");
 			let recipesCollection = ``;
 
+			// Iterate through each recipe, limit to the first 6 recipes
 			recipes.forEach((recipe, index) => {
-				if (index > 5) return;
+				if (index > 5) return; // Skip recipes beyond the first 6
 
+				// Build HTML markup for each recipe card
 				recipesCollection += `<div class="col-lg-4">
 				<div class="right-first-image">
 					<div class="img-shadow">
@@ -37,20 +42,23 @@ $(() => {
 			</div>`;
 			});
 
+			// Set the HTML content of the recipes container
 			recipesElement.html(recipesCollection);
-		}).catch(err => console.error(err));
+		})
+		.catch(err => console.error(err)); // Handle any errors that occur during the fetch operation
 
+	// Handle click event on "Explore More" button
 	$("#explore-more").on("click", () => {
-		console.log("click called ::");
-
+		// Fetch recipes again when "Explore More" is clicked
 		fetch("./json/recipes.json")
 			.then(response => response.json())
 			.then(recipes => {
+				// Select the recipes container element
 				const recipesElement = $("#recipes");
 				let recipesCollection = ``;
 
+				// Iterate through each recipe and build HTML markup for each recipe card
 				recipes.forEach((recipe, index) => {
-
 					recipesCollection += `<div class="col-lg-4">
 				<div class="right-first-image">
 					<div class="img-shadow">
@@ -78,9 +86,12 @@ $(() => {
 			</div>`;
 				});
 
+				// Set the HTML content of the recipes container
 				recipesElement.html(recipesCollection);
 
+				// Hide the "Explore More" button after loading additional recipes
 				$("#explore-more").hide();
-			}).catch(err => console.error(err));
+			})
+			.catch(err => console.error(err)); // Handle any errors that occur during the fetch operation
 	});
 });
